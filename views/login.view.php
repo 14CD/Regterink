e06cddcaaaa3a8aefa9f06290c8a9612c033317e
aaronweggemans <aaronweggemans@hotmail.nl>
2018-10-09 15:19:52 +0200

    Added files.

---
+++ views/login.view.php
@@ -0,0 +1,45 @@
+<?php require 'partials/head.php'; ?>
+    <header class="mastheadLogin text-white">
+        <div class="mask text-center d-flex">
+            <div class="container my-auto">
+                <div class="row">
+                    <div class="col-lg-10 mx-auto">
+                        <h2>Login</h2>
+                        <form action="" method="post">
+                            <div class="row mt-2">
+                                <div class="col-md-4">
+                                    <h5>Gebruikersnaam:</h5>
+                                </div>
+                                <div class="col-md-6">
+                                    <input type="text" class="form-control">
+                                </div>
+                            </div>
+                            <div class="row mt-2">
+                                <div class="col-md-4">
+                                    <h5>Password:</h5>
+                                </div>
+                                <div class="col-md-6">
+                                    <input type="password" class="form-control">
+                                </div>
+                            </div>
+                            <div class="row mt-2">
+                                <div class="col-md-4">
+
+                                </div>
+                                <div class="col-md-6" align="right">
+                                    <input type="submit" value="Login" class="btn btn-primary">
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Custom scripts for this template -->
+    <script src="site-assets/js/creative.min.js"></script>
+<?php require 'partials/foot.php'; ?>
