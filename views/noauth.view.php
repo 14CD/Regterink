377505837587bebf3e131ba3f94932f5b3743b12
aaronweggemans <aaronweggemans@hotmail.nl>
2018-10-10 21:20:20 +0200

    auth view

---
+++ views/noauth.view.php
@@ -0,0 +1,20 @@
+<?php require 'partials/head.php'; ?>
+    <header class="mastheadDanger text-white">
+        <div class="mask text-center d-flex">
+            <div class="container my-auto">
+                <div class="row">
+                    <div class="col-lg-10 mx-auto">
+                        <form action="" method="post">
+                            <div class="row mt-2">
+                                <div class="col-md-12">
+                                    <h1>U heeft geen toegang tot deze pagina</h1>
+                                    <p>Onze excuses voor het ongemakt</p>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+<?php require 'partials/foot.php'; ?>
