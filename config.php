14b31005cefc66b90b560295d53c2783aaf63b4c
unknown <k.rebien@mail.com>
2018-10-10 19:29:10 +0200

    mijn config voor de test

--- config.php
+++ config.php
@@ -2,10 +2,10 @@
 
 return [
     'database' => [
-        'name' => 'regterink',
-        'username' => 'regterink',
-        'password' => 'regterink',
-        'connection' => 'mysql:host=regterink-mysql',
+        'name' => 'db_regterink',
+        'username' => 'root',
+        'password' => 'root',
+        'connection' => 'mysql:host=localhost-mysql',
         'options' => [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING
         ]
