48b1c17f3fa710581ece5b3c9a15af81dfe6bb1b
aaronweggemans <aaronweggemans@hotmail.nl>
2018-10-15 21:10:13 +0200

    verzorgende pagina

---
+++ controllers/nuturing.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: aaronweggemans
+ * Date: 15/10/2018
+ * Time: 21:07
+ */
+
+require "views/admin/nuturings.view.php";
---
+++ views/admin/nuturings.view.php
@@ -0,0 +1,32 @@
+<?php require "partials/head.php" ?>
+<?php require "partials/nav.php" ?>
+    <div id="wrapper">
+        <?php require "partials/sidebar.php" ?>
+        <div id="content-wrapper">
+            <div class="container-fluid">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item">
+                        Dashboard
+                    </li>
+                    <li class="breadcrumb-item active">Gebruikers</li>
+                </ol>
+
+                <div class="card mb-3">
+                    <div class="card-header">
+                        Verzorgende
+                    </div>
+                    <div class="card-body">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus amet aperiam aut
+                            consequatur dicta dolor earum ex fuga harum hic illo iure labore laboriosam maxime modi nam
+                            natus officiis quae quisquam, quod ratione suscipit tempora totam unde veniam vero voluptas
+                            voluptatum. Ab, aliquam autem delectus ducimus ea earum eius error facere, facilis harum id
+                            illo incidunt iure, laborum libero magnam maxime molestias nisi nobis nostrum odio quasi
+                            ratione repellat reprehenderit sunt tenetur unde voluptate. A debitis, dolore nulla officia
+                            saepe sint soluta tempora voluptatibus. A animi consectetur non. Accusantium aperiam at
+                            dicta error laboriosam minus odio soluta tempora veniam voluptatum.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+<?php require "partials/foot.php" ?>
