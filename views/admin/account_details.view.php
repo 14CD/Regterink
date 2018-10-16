69c53cbef6cc3de9bcd8d54432c8a63677f08bcb
Navid Haghighi <navidhaghighi.1900@gmail.com>
2018-10-16 14:42:27 +0200

    user info aanpassingen

--- views/admin/account_details.view.php
+++ views/admin/account_details.view.php
@@ -1,24 +1,62 @@
 <?php require "partials/head.php" ?>
 <?php require "partials/nav.php" ?>
-<div id="wrapper">
-    <?php require "partials/sidebar.php" ?>
-    <div id="content-wrapper">
-        <div class="container-fluid">
-            <ol class="breadcrumb">
-                <li class="breadcrumb-item">
-                    Dashboard
-                </li>
-                <li class="breadcrumb-item active">Account</li>
-            </ol>
-            <div class="card mb-3">
-                <div class="card-header">
-                    Uw gegevens
-                </div>
-                <div class="card-body">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus amet aperiam aut consequatur dicta dolor earum ex fuga harum hic illo iure labore laboriosam maxime modi nam natus officiis quae quisquam, quod ratione suscipit tempora totam unde veniam vero voluptas voluptatum. Ab, aliquam autem delectus ducimus ea earum eius error facere, facilis harum id illo incidunt iure, laborum libero magnam maxime molestias nisi nobis nostrum odio quasi ratione repellat reprehenderit sunt tenetur unde voluptate. A debitis, dolore nulla officia saepe sint soluta tempora voluptatibus. A animi consectetur non. Accusantium aperiam at dicta error laboriosam minus odio soluta tempora veniam voluptatum.</p>
+    <div id="wrapper">
+        <?php require "partials/sidebar.php" ?>
+        <div id="content-wrapper">
+            <div class="container-fluid">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item">
+                        Dashboard
+                    </li>
+                    <li class="breadcrumb-item active">Account</li>
+                </ol>
+                <div class="card mb-3">
+                    <div class="card-header">
+                        Uw gegevens
+                    </div>
+                    <div class="card-body">
+                        <form action="" method="post">
+                            <div class="row">
+                                <div class="col-md-8">
+                                    <table class="table table-striped">
+                                        <tbody>
+                                        <tr>
+                                            <td>username:</td>
+                                            <td>
+                                                <input type="text" class="form-control" value="Navid Haghighi">
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>email:</td>
+                                            <td>
+                                                <input type="text" class="form-control" value="navidhaghighi.1900@gmail.com">
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>mobiel:</td>
+                                            <td>
+                                                <input type="text" class="form-control" value="06-10202809">
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>wachtwoord:</td>
+                                            <td>
+                                                <input type="password" class="form-control" value="geheimeDingen">
+                                            </td>
+                                        </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                                <div class="col-md-4">
+                                    <img src="https://images-na.ssl-images-amazon.com/images/S/sgp-catalog-images/region_US/wb-883316843024-Full-Image_GalleryBackground-en-US-1483994511403._RI_SX940_.jpg"
+                                         alt="" class="img-thumbnail">
+                                </div>
+                                <input class="btn btn-primary" type="submit" value="Submit">
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 <?php require "partials/foot.php" ?>
