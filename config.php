123b80cbccb20fcfe2808f9a32ca6e406ee217f3
aaronweggemans <aaronweggemans@hotmail.nl>
2018-10-16 14:49:09 +0200

    check

--- config.php
+++ config.php
@@ -12,7 +12,7 @@ return [
     'database' => [
         'name' => 'db_regterink',
         'username' => 'root',
-        'password' => '24Oktober!',
+        'password' => 'rootroot',
         'connection' => 'mysql:host=127.0.0.1',
         'options' => [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING
