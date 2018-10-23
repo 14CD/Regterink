c6f7c164e44978beedc938abb2064fcbfa59ff73
aaronweggemans <aaronweggemans@hotmail.nl>
2018-10-23 17:16:31 +0200

    test

--- routes.php
+++ routes.php
@@ -3,31 +3,31 @@
  * GET routes
  */
 
+//$_SESSION['AdminLogin']
+//$_SESSION['VerzorgendeLogin']
+//$_SESSION['OuderLogin']
+//$_SESSION['KindLogin']
+
 $router->get('', 'controllers/index.php');
 $router->get('home', 'controllers/index.php');
-$router->get('login', 'controllers/login.php');
+
+$router->post('login_user', 'controllers/post/login_user.php');
+
 $router->get('new_password', 'controllers/change_password.php');
 $router->get('children', 'controllers/children.php');
-
 $router->get('dashboard', 'controllers/dashboard.php');
 $router->get('user', 'controllers/users.php');
-
 $router->get('add_user', 'controllers/new_user.php');
-
 $router->get('account_details', 'controllers/account_details.php');
-
 $router->get('nuturing', 'controllers/nuturing.php');
 $router->get('documents', 'controllers/get/documents/list_documents.php');
 
-/**
- * POST routes
- */
 $router->post('/add_player', 'controllers/add_player.php');
 $router->post('post_add_user', 'controllers/post/add_user.php');
-$router->post('login_user', 'controllers/post/login_user.php');
 $router->post('sendmail', 'controllers/post/send_mail.php');
-
 $router->post('change_details', 'controllers/post/change_details.php');
 
 //Remove actions
 $router->post('post_remove_user', 'controllers/post/post_remove_user.php');
+
+$router->get('login', 'controllers/login.php');
