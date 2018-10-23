215211d2bd71da3627e43ef2332444c071185094
Onno K <onnok98@gmail.com>
2018-10-23 15:25:12 +0200

    Added sendmail to the routes.

--- routes.php
+++ routes.php
@@ -17,12 +17,14 @@ $router->get('add_user', 'controllers/new_user.php');
 $router->get('account_details', 'controllers/account_details.php');
 
 $router->get('nuturing', 'controllers/nuturing.php');
+
 /**
  * POST routes
  */
 $router->post('/add_player', 'controllers/add_player.php');
 $router->post('post_add_user', 'controllers/post/add_user.php');
 $router->post('login_user', 'controllers/post/login_user.php');
+$router->post('sendmail', 'controllers/post/sendmail.php');
 
 //Remove actions
 $router->get('post_remove_user', 'controllers/post/post_remove_user.php');
